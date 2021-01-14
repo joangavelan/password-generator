a76f64f50dc10cb7f55a01f120c697a7f15382ef
Johan Gavelan <johangavelan@gmail.com>
2021-01-14 03:22:47 -0500

    project done

--- app.js
+++ app.js
@@ -1,7 +1,85 @@
-const range = document.querySelector('.range');
-const rangeFill = document.querySelector('.fill');
+const App = (() => {
+    const passwordEl = document.querySelector('.password');
+    const clipboardEl = document.querySelector('.clipboard');
+    const passwordLengthEl = document.querySelector('.password-length');
+    const rangeEl = document.querySelector('.range');
+    const rangeFillEl = document.querySelector('.fill');
+    const settingsEl = document.getElementsByClassName('settings');
+    const generatePasswordBtn = document.querySelector('.btn');
+    
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    const numbers = '123456789';
+    
+    const ranIndex = (str) => str[Math.floor(Math.random() * str.length)];
+    
+    const getLowercaseLetter = () => ranIndex(letters).toLowerCase();
+    const getUppercaseLetter = () => ranIndex(letters).toUpperCase();
+    const getNumber = () => ranIndex(numbers);
+    const getSymbol = () => ranIndex(symbols);
+    
+    const getCharacter = (setting) => {
+        switch(setting) {
+            case 'numbers':
+                return getNumber();
+            case 'lowercase':
+                return getLowercaseLetter();
+            case 'uppercase':
+                return getUppercaseLetter();
+            case 'symbols':
+                return getSymbol();
+            default:
+                console.log('invalid setting');
+        }
+    }
 
-range.addEventListener('input', event => {
-    //progress effect
-    rangeFill.style.width = `${range.value}%`;
-})
+    const rangeValueRender = () => {
+        const rangeValue = Math.floor((rangeEl.value / 6) + 4);
+        passwordLengthEl.textContent = rangeValue;
+        rangeFillEl.style.width = `${rangeEl.value}%`;    
+    }
+    
+    const generatePassword = () => {
+        let password = '';
+        const passwordLength = +passwordLengthEl.textContent;
+        const enabledSettings = [];
+        for(let setting of settingsEl) {
+            if(setting.checked === true) enabledSettings.push(setting.name);
+        }
+    
+        if(enabledSettings.length >= 1) {
+            for(let i = 0; i < passwordLength; i++) {
+                password += getCharacter(ranIndex(enabledSettings));
+            }
+            passwordEl.textContent = password;
+        }
+        else alert('You must enable at least 1 setting')
+    }
+    
+    const copyToClipboard = htmlElement => {
+        if(!htmlElement) return;
+        const elementText = htmlElement.textContent;
+        const inputElement = document.createElement('input');
+        inputElement.setAttribute('value', elementText);
+        document.body.appendChild(inputElement);
+        inputElement.select();
+        document.execCommand('copy');
+        inputElement.parentNode.removeChild(inputElement);
+    }
+
+    const listeners = () => {
+        generatePasswordBtn.addEventListener('click', generatePassword);
+        clipboardEl.addEventListener('click', () => copyToClipboard(passwordEl));
+        rangeEl.addEventListener('input', rangeValueRender);
+    }
+
+
+    return {
+        rangeValueRender,
+        listeners
+    }
+
+})();
+
+App.rangeValueRender();
+App.listeners();
